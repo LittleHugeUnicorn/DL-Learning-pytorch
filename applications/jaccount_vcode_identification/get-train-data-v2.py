4f4ee52b6cb3d3ebff5224afb46a17e1dab7ac73
UNIkeEN <2641712741@qq.com>
2022-07-11 14:31:45 +0800

    .

---
+++ applications/jaccount_vcode_identification/get-train-data-v2.py
@@ -0,0 +1,25 @@
+import requests
+import os
+import ddddocr
+if not os.path.exists('./验证码'):
+    os.mkdir('./验证码')
+headers={
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36'
+}
+url = 'https://jaccount.sjtu.edu.cn/jaccount/captcha?uuid=e956d943-49c2-45f4-991d-aef34605c890&t=1657440018672'
+
+for i in range(1,2):
+    img_data = requests.get(url=url,headers=headers).content
+    filename = str(i)+'.jpg'
+    filepath = './验证码/'+filename
+    with open(filepath,'wb') as fp:
+        fp.write(img_data)
+    ocr = ddddocr.DdddOcr()
+    with open(filepath,'rb') as f:
+        img_bytes = f.read()
+    res = ocr.classification(img_bytes)
+    newpath = './验证码/'+str(res)+'.jpg'
+    print(res)
+    os.rename(filepath,newpath)
+print('完毕')
+
